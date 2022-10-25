8a8447bc8014356bf22bcdff3dd2d22c6f5c3b19
SSH9753 <kor159753@korea.ac.kr>
2022-10-25 15:37:07 +0900

    modify logger

--- logger.py
+++ logger.py
@@ -1,6 +1,3 @@
-from atexit import register
-from distutils.sysconfig import customize_compiler
-from click import argument
 from unicorn import *
 from unicorn.x86_const import *
 import logging
@@ -9,16 +6,39 @@ import struct
 from capstone import *
 import lief  # type: ignore
 
-regis = {"RAX": UC_X86_REG_RAX ,"RBX": UC_X86_REG_RBX,"RCX": UC_X86_REG_RCX,"RDX": UC_X86_REG_RDX,"RDI": UC_X86_REG_RDI,"RSI": UC_X86_REG_RSI,"R8": UC_X86_REG_R8,
-"R9": UC_X86_REG_R9,"R10": UC_X86_REG_R10,"R11": UC_X86_REG_R11,"R12": UC_X86_REG_R12,"R13": UC_X86_REG_R13,"R14": UC_X86_REG_R14,"R15": UC_X86_REG_R15,"RBP": UC_X86_REG_RBP,"RSP": UC_X86_REG_RSP,"RIP": UC_X86_REG_RIP}
+regis = {
+    "RAX": UC_X86_REG_RAX,
+    "RBX": UC_X86_REG_RBX,
+    "RCX": UC_X86_REG_RCX,
+    "RDX": UC_X86_REG_RDX,
+    "RDI": UC_X86_REG_RDI,
+    "RSI": UC_X86_REG_RSI,
+    "R8": UC_X86_REG_R8, 
+    "R9": UC_X86_REG_R9,
+    "R10": UC_X86_REG_R10,
+    "R11": UC_X86_REG_R11,
+    "R12": UC_X86_REG_R12,
+    "R13": UC_X86_REG_R13,
+    "R14": UC_X86_REG_R14,
+    "R15": UC_X86_REG_R15,
+    "RBP": UC_X86_REG_RBP,
+    "RSP": UC_X86_REG_RSP,
+    "RIP": UC_X86_REG_RIP
+    }
 
 def disas(code,address):
     md=Cs(CS_ARCH_X86,CS_MODE_64)
     assem=md.disasm(code,address)
     return assem
 
-def setup_logger(uc,logger: logging.Logger) -> None:
+def setup_logger(uc,logger: logging.Logger, verbose:bool) -> None:
     
+    lief.logging.disable()
+    if verbose:
+        logLevel = logging.DEBUG
+    else:
+        logLevel = logging.INFO
+
 
     logger.setLevel(logging.DEBUG)
 
