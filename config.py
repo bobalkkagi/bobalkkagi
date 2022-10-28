a11fbb0755afe186eb333c6ecdd3b5478b8a5ea1
hackerhoon <gkswlgns21@gmail.com>
2022-10-28 17:08:37 +0900

    add

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ class DLL_SETTING:
     DLL_FUNCTIONS = {} # {function : address}
     LOADED_DLL = {} # {dll : address}
     
-    def InvDict():
-        INV_DLL_FUNCTIONS = {v: k for k, v in DLL_FUNCTIONS.items()}
-        INV_LOADED_DLL = {v: k for k, v in LOADED_DLL.items()}
+    def InvDict(self):
+        self.INV_DLL_FUNCTIONS = {v: k for k, v in DLL_FUNCTIONS.items()}
+        self.INV_LOADED_DLL = {v: k for k, v in LOADED_DLL.items()}
 
 def i_queue(data):
     globar_var.a_queue.insert(0,data)
