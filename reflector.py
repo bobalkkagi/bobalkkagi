a76f3578c93cf6e4ec094d7440e7288fe4b670a6
hoon12 <37144958+hackerhoon@users.noreply.github.com>
2022-11-18 00:43:18 +0900

    Add files via upload

---
+++ reflector.py
@@ -0,0 +1,165 @@
+REFLECTOR = {
+        "api-ms-win-core-sysinfo-l1-1-0.dll" : "kernelbase.dll",
+        "api-ms-win-core-libraryloader-l1-2-0.dll" : "kernelbase.dll",
+        "api-ms-win-core-errorhandling-l1-1-0.dll" : "kernelbase.dll",
+        "api-ms-win-core-libraryloader-l1-1-0.dll" : "kernelbase.dll",
+        "api-ms-win-crt-runtime-l1-1-0.dll" : "ucrtbase.dll",
+        "api-ms-win-crt-heap-l1-1-0.dll" : "ucrtbase.dll",
+        "api-ms-win-crt-stdio-l1-1-0.dll" : "ucrtbase.dll",
+        "api-ms-win-crt-locale-l1-1-0.dll" : "ucrtbase.dll",
+        "api-ms-win-core-synch-l1-1-0.dll" : "ntdll.dll",
+        "api-ms-win-core-heap-l1-1-0.dll" : "ntdll.dll",
+        'api-ms-win-eventing-provider-l1-1-0.dll': 'advapi32.dll',
+        'api-ms-win-core-processthreads-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-processthreads-l1-1-3.dll': 'kernel32.dll',
+        'api-ms-win-core-processthreads-l1-1-2.dll': 'kernel32.dll',
+        'api-ms-win-core-processthreads-l1-1-1.dll': 'kernel32.dll',
+        'api-ms-win-core-heap-l2-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-memory-l1-1-1.dll': 'kernel32.dll',
+        'api-ms-win-core-memory-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-memory-l1-1-2.dll': 'kernel32.dll',
+        'api-ms-win-core-handle-l1-1-0.dll': 'kernelbase.dll',
+        'api-ms-win-core-synch-l1-2-1.dll': 'kernel32.dll',
+        'api-ms-win-core-synch-l1-2-0.dll': 'kernel32.dll',
+        'api-ms-win-core-file-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-file-l1-2-2.dll': 'kernel32.dll',
+        'api-ms-win-core-file-l1-2-1.dll': 'kernel32.dll',
+        'api-ms-win-core-io-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-io-l1-1-1.dll': 'kernel32.dll',
+        'api-ms-win-core-largeinteger-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-libraryloader-l1-2-1.dll': "kernelbase.dll",
+        'api-ms-win-core-libraryloader-l2-1-0.dll': "kernelbase.dll",
+        'api-ms-win-core-namedpipe-l1-2-2.dll': 'kernel32.dll',
+        'api-ms-win-core-namedpipe-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-namedpipe-l1-2-1.dll': 'kernel32.dll',
+        'api-ms-win-core-datetime-l1-1-1.dll': 'kernel32.dll',
+        'api-ms-win-core-datetime-l1-1-2.dll': 'kernel32.dll',
+        'api-ms-win-core-sysinfo-l1-2-0.dll': 'kernel32.dll',
+        'api-ms-win-core-sysinfo-l1-2-1.dll': 'kernel32.dll',
+        'api-ms-win-core-sysinfo-l1-2-3.dll': 'kernelbase.dll',
+        'api-ms-win-core-localization-l1-2-0.dll': 'kernel32.dll',
+        'api-ms-win-core-processenvironment-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-processenvironment-l1-2-0.dll': 'kernel32.dll',
+        'api-ms-win-core-string-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-debug-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-debug-l1-1-1.dll': 'kernel32.dll',
+        'api-ms-win-core-errorhandling-l1-1-3.dll': 'kernel32.dll',
+        'api-ms-win-core-util-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-profile-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-security-base-l1-1-0.dll': 'advapi32.dll',
+        'api-ms-win-security-base-l1-2-0.dll': 'kernel32.dll',
+        'api-ms-win-core-comm-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-realtime-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-wow64-l1-1-1.dll': 'kernel32.dll',
+        'api-ms-win-core-wow64-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-namespace-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-file-l2-1-2.dll': 'kernel32.dll',
+        'api-ms-win-core-file-l2-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-file-l2-1-1.dll': 'kernel32.dll',
+        'api-ms-win-core-xstate-l2-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-localization-l2-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-normalization-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-windowserrorreporting-l1-1-1.dll': 'kernel32.dll',
+        'api-ms-win-core-windowserrorreporting-l1-1-2.dll': 'kernelbase.dll',
+        'api-ms-win-core-windowserrorreporting-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-console-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-console-l1-2-0.dll': 'kernel32.dll',
+        'api-ms-win-core-console-l2-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-console-l2-2-0.dll': 'kernel32.dll',
+        'api-ms-win-core-console-l3-2-0.dll': 'kernel32.dll',
+        'api-ms-win-core-psapi-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-psapi-ansi-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-delayload-l1-1-1.dll': 'kernel32.dll',
+        'api-ms-win-core-threadpool-l1-2-0.dll': 'kernel32.dll',
+        'api-ms-win-core-memory-l1-1-3.dll': 'kernel32.dll',
+        'api-ms-win-core-heap-obsolete-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-localization-obsolete-l1-2-0.dll': 'kernel32.dll',
+        'api-ms-win-core-kernel32-legacy-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-kernel32-legacy-l1-1-1.dll': 'kernel32.dll',
+        'api-ms-win-core-interlocked-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-eventing-controller-l1-1-0.dll': 'advapi32.dll',
+        'api-ms-win-eventing-consumer-l1-1-0.dll': 'advapi32.dll',
+        'api-ms-win-core-localization-l1-2-2.dll': 'kernel32.dll',
+        'api-ms-win-eventing-classicprovider-l1-1-0.dll': 'advapi32.dll',
+        'api-ms-win-core-url-l1-1-0.dll': 'shlwapi.dll',
+         # Guessing to match dll
+        'api-ms-win-core-rtlsupport-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-rtlsupport-l1-2-2.dll': 'kernel32.dll',
+        'api-ms-win-core-apiquery-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-registry-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-file-l1-2-0.dll': 'kernel32.dll',
+        'api-ms-win-core-file-l1-2-4.dll': 'kernel32.dll',
+        'api-ms-win-core-delayload-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-job-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-threadpool-legacy-l1-1-0.dll': 'kernelbase.dll',
+        'api-ms-win-core-threadpool-private-l1-1-0.dll': 'kernelbase.dll',
+        'api-ms-win-core-libraryloader-l1-2-3.dll': 'kernel32.dll',
+        'api-ms-win-core-libraryloader-l1-2-2.dll': 'kernel32.dll',
+        'api-ms-win-core-datetime-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-timezone-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-processsnapshot-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-fibers-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-security-appcontainer-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-wow64-l1-1-3.dll': 'kernel32.dll',
+        'api-ms-win-core-systemtopology-l1-1-1.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-systemtopology-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-processtopology-l1-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-file-l2-1-3.dll': 'kernel32.dll',
+        'api-ms-win-core-xstate-l2-1-1.dll': 'kernel32.dll',
+        'api-ms-win-core-xstate-l2-1-2.dll': 'kernel32.dll',
+        'api-ms-win-core-fibers-l2-1-0.dll': 'kernel32.dll',
+        'api-ms-win-core-fibers-l2-1-1.dll': 'kernel32.dll',
+        'api-ms-win-core-localization-private-l1-1-0.dll':'kernel32.dll',
+        'api-ms-win-core-sidebyside-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-appcompat-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-windowserrorreporting-l1-1-3.dll': 'kernel32.dll',
+        'api-ms-win-core-console-l1-2-1.dll': 'kernel32.dll',
+        'api-ms-win-core-appcompat-l1-1-1.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-errorhandling-l1-1-2.dll': 'kernelbase.dll',
+        'api-ms-win-core-string-l2-1-0.dll': 'kernelbase.dll',
+        'api-ms-win-core-privateprofile-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-atoms-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-string-obsolete-l1-1-0.dll': 'kernelbase.dll',
+        'api-ms-win-core-stringansi-l1-1-0.dll': 'kernelbase.dll', #can not find!
+        'api-ms-win-core-kernel32-private-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-appinit-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-gdi-internal-uap-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-stateseparation-helpers-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-apiquery-l2-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-crt-string-l1-1-0.dll': 'kernelbase.dll',
+        'api-ms-win-crt-convert-l1-1-0.dll': "ucrtbase.dll",
+        'api-ms-win-crt-utility-l1-1-0.dll': "ucrtbase.dll",
+        'api-ms-win-crt-time-l1-1-0.dll': "ucrtbase.dll",
+        'api-ms-win-crt-math-l1-1-0.dll': "ucrtbase.dll",
+        'api-ms-win-eventing-consumer-l1-1-1.dll': 'advapi32.dll',
+        'api-ms-win-core-crt-l1-1-0.dll': "ucrtbase.dll",
+        'api-ms-win-core-crt-l2-1-0.dll': "ucrtbase.dll",
+        'api-ms-win-service-core-l1-1-0.dll': 'advapi32.dll',
+        'api-ms-win-service-core-l1-1-1.dll': 'advapi32.dll',
+        'api-ms-win-service-core-l1-1-2.dll': 'advapi32.dll',
+        'api-ms-win-service-management-l1-1-0.dll': 'advapi32.dll',
+        'api-ms-win-service-management-l2-1-0.dll': 'advapi32.dll',
+        'api-ms-win-service-private-l1-1-4.dll': 'advapi32.dll',
+        'api-ms-win-service-private-l1-1-2.dll': 'advapi32.dll',
+        'api-ms-win-service-private-l1-1-3.dll': 'advapi32.dll',
+        'api-ms-win-service-private-l1-1-0.dll': 'advapi32.dll',
+        'api-ms-win-service-winsvc-l1-1-0.dll': 'advapi32.dll',
+        'api-ms-win-security-base-private-l1-1-0.dll': 'advapi32.dll',
+        'api-ms-win-core-registry-l1-1-1.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-registry-l1-1-2.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-misc-l1-1-0.dll': 'kernelbase.dll',
+        'api-ms-win-security-audit-l1-1-1.dll': 'advapi32.dll', #can not find!
+        'api-ms-win-security-audit-l1-1-0.dll': 'advapi32.dll',  #can not find!
+        'api-ms-win-core-pcw-l1-1-0.dll': 'kernelbase.dll', #can not find!
+        'api-ms-win-crt-private-l1-1-0.dll':"ucrtbase.dll", #can not find!
+        'api-ms-win-core-path-l1-1-0.dll': 'kernelbase.dll', #can not find!
+        'api-ms-win-core-string-l2-1-1.dll': 'kernelbase.dll',
+        'api-ms-win-core-version-l1-1-0.dll': 'version.dll', #can not find!
+        'api-ms-win-core-shlwapi-obsolete-l1-1-0.dll': 'shlwapi.dll', #can not find!
+        'api-ms-win-core-shlwapi-legacy-l1-1-0.dll': 'shlwapi.dll', #can not find!
+        'api-ms-win-core-kernel32-legacy-l1-1-2.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-registryuserspecific-l1-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-shell-shellcom-l1-1-0.dll': 'shell32.dll', #can not find!
+        'api-ms-win-core-job-l2-1-0.dll': 'kernel32.dll', #can not find!
+        'api-ms-win-core-versionansi-l1-1-0.dll': 'version.dll', #can not find!
+ }
