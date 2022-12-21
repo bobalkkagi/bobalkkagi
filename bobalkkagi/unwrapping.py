e4c00d1e994576d63e5232a3a02b8970b66dd90a
hackerhoon <gkswlgns21@gmail.com>
2022-12-21 14:06:55 +0900

    'dump_restart-> unwrapping'

--- bobalkkagi/unwrapping.py
+++ bobalkkagi/unwrapping.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
-from .util_unwrap import dump_restart
+from .util_unwrap import unwrapping
 
 def unwrap(dumps, OEP:int):
     start = datetime.now()
     print(f"\033[93m[{start}] Unwrapping Start...\033[0m")
-    dump_restart(dumps, OEP)
+    unwrapping(dumps, OEP)
     end = datetime.now()
     print(f"\033[93m[{end}] Unwrapping End...\033[0m")
     print(f"\033[93mUnwrapping Runtime: [{end-start}]\033[0m")
